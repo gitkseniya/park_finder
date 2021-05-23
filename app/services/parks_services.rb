9a0ebcc65983b82982c1c0cf8522a80e3c559e88
Kseniya K <kseniya.kudelich@gmail.com>
2021-05-22 18:14:19 -0600

    My method is working fine

--- app/services/parks_services.rb
+++ app/services/parks_services.rb
@@ -3,19 +3,15 @@ class ParksServices
   def self.get_details(state)
     api_key = ENV['api_key']
 
+    conn = Faraday.new(url: "https://developer.nps.gov")
+
     response = conn.get("/api/v1/parks?stateCode=#{state}&api_key=#{api_key}")
 
-    parse_info(response)[:data]
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    json[:data]
   end
 
-  def self.conn
-    Faraday.new(url: "https://developer.nps.gov")
-  end
 
-  def self.parse_info(response)
-    JSON.parse(response.body, symbolize_names: true)
-
-
-  end
 
 end
